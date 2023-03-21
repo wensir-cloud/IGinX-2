15a75acc212c7d7ec7282b870419e1b997347833
SolomonAnn <thss15_anyz@163.com>
2023-03-21 15:13:47 +0800

    Run another pg

--- .github/postgresql.sh
+++ .github/postgresql.sh
@@ -26,4 +26,20 @@ sh -c "sudo su - postgres -c '/usr/lib/postgresql/15/bin/initdb -D /var/lib/post
 
 sh -c "sudo su - postgres -c '/usr/lib/postgresql/15/bin/pg_ctl -D /var/lib/postgresql/15/main start'"
 
-sh -c "sudo su - postgres -c 'psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
+sh -c "sudo su - postgres -c '/usr/lib/postgresql/15/bin/psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
+
+sh -c "sudo mkdir -p /usr/lib/postgresql2/15"
+
+sh -c "sudo cp -r /usr/lib/postgresql/15 /usr/lib/postgresql2/15"
+
+sh -c "sudo mkdir -p /var/lib/postgresql2/15/main"
+
+sh -c "sudo chown -R postgres /var/lib/postgresql2/15/main"
+
+sh -c "sudo chmod -R 777 /var/lib/postgresql2/15/main"
+
+sh -c "sudo su - postgres -c '/usr/lib/postgresql2/15/bin/initdb -D /var/lib/postgresql2/15/main --auth-local peer --auth-host scram-sha-256 --no-instructions'"
+
+sh -c "sudo su - postgres -c '/usr/lib/postgresql2/15/bin/pg_ctl -D /var/lib/postgresql2/15/main -o \"-F -p 5433\" start'"
+
+sh -c "sudo su - postgres -c '/usr/lib/postgresql2/15/bin/psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
