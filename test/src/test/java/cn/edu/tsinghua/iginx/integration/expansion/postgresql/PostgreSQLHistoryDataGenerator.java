ef236d13b7ab9243b7e2019b18f48403bb1ed5bd
SolomonAnn <thss15_anyz@163.com>
2023-03-21 16:37:35 +0800

    Run another pg

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/postgresql/PostgreSQLHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/postgresql/PostgreSQLHistoryDataGenerator.java
@@ -34,7 +34,7 @@ public class PostgreSQLHistoryDataGenerator extends BaseHistoryDataGenerator {
 
     private Connection connect(int port) {
         try {
-            String url = String.format("jdbc:postgresql://127.0.0.1:%d", port);
+            String url = String.format("jdbc:postgresql://127.0.0.1:%d/", port);
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(url, USERNAME, PASSWORD);
         } catch (SQLException | ClassNotFoundException e) {
